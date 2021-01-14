fec5dd6a13e440651a5cca8778bab05cef20c21e
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-13 23:31:13 -0600

    jumbotron update

--- userdir/src/components/jumbotron/index.js
+++ userdir/src/components/jumbotron/index.js
@@ -4,7 +4,6 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron">
             <h1 className="display-4">Employee Directory</h1>
-            <p className="lead">Search for an employee or sort by name.</p>
             <hr className="my-4"/>
             
         </div>
